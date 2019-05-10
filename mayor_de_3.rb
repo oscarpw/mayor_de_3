04907ff996ad7f0904f9d5b4dae3a91ba6c652f6
oscarpw <oscar.castillo@primeraweb.cl>
2019-05-09 21:25:01 -0400

    numero mayor de 3 final

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -1,16 +1,14 @@
 ingreso_datos=ARGV
-n1=ingreso_datos(0).to_i
-n2=ingreso_datos(1).to_i
-n3=ingreso_datos(2).to_i
+n1=ingreso_datos[0].to_i
+n2=ingreso_datos[1].to_i
+n3=ingreso_datos[2].to_i
 
 if n1 > n2
     if n1 > n3
         puts "El número mayor es #{n1}"
-    else
-        puts "El número mayor es #{n3}"
-end
-if n2 > n3
+    end
+elsif n2 > n3
     puts "El número mayor es #{n2}"
-else
+    elsif
     puts "El número mayor es #{n3}"
 end
