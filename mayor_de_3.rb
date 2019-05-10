64a19432298601b41e28c64bfb3fed1516754bc3
oscarpw <oscar.castillo@primeraweb.cl>
2019-05-09 21:49:27 -0400

    mayor de 3 corregido final

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -3,12 +3,11 @@ n1=ingreso_datos[0].to_i
 n2=ingreso_datos[1].to_i
 n3=ingreso_datos[2].to_i
 
-if n1 > n2
-    if n1 > n3
-        puts "El número mayor es #{n1}"
-    end
-elsif n2 > n3
+
+if n1 > n2 && n1 > n3
+    puts "El número mayor es #{n1}"
+elsif n2 > n1 && n2 > n3
     puts "El número mayor es #{n2}"
-    elsif
+elsif n3 > n1 && n3 > n2
     puts "El número mayor es #{n3}"
 end
