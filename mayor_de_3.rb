2817681721cc65461526bd9ac36c03b153825989
oscarpw <oscar.castillo@primeraweb.cl>
2019-05-09 20:23:45 -0400

    numero mayor primera version

---
+++ mayor_de_3.rb
@@ -0,0 +1,16 @@
+ingreso_datos=ARGV
+n1=ingreso_datos(0).to_i
+n2=ingreso_datos(1).to_i
+n3=ingreso_datos(2).to_i
+
+if n1 > n2
+    if n1 > n3
+        puts "El número mayor es #{n1}"
+    else
+        puts "El número mayor es #{n3}"
+end
+if n2 > n3
+    puts "El número mayor es #{n2}"
+else
+    puts "El número mayor es #{n3}"
+end
